9697dd287eeef172d098cf5705b5d208cede5c94
e341wang <112414871+e341wang@users.noreply.github.com>
2022-12-01 09:16:15 -0800

    Update remote_setup.sh

--- src/nimbo/scripts/remote_setup.sh
+++ src/nimbo/scripts/remote_setup.sh
@@ -96,7 +96,7 @@ elif [ "$JOB_CMD" = "_nimbo_notebook" ]; then
     echo "Notebook running at http://localhost:57467/lab"
     exit 0
 else
-    echo "Running job: ${@:2}"
+    echo "Running job: $2"
     echo $JOB_CMD
     eval ${@:2}
 fi
